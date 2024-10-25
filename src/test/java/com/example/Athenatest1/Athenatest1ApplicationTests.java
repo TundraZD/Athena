9b78ad6d14d9f60fb0a4134c5cb90010741f9f4b
Tundra <pui83613@gmail.com>
2024-10-25 11:38:14 +0300

    test2

---
+++ src/test/java/com/example/Athenatest1/Athenatest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Athenatest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Athenatest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
